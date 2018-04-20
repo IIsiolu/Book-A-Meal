f0661f18ea50cceb02022246c67d079272d0df6c
phemy <phemy.smith@gmail.com>
2018-04-20 14:40:01 +0100

    targeting bookings

---
+++ UI/js/home.js
@@ -0,0 +1,15 @@
+const order= (input, sent) => {
+	console.log(input)
+	let dom = document.createElement('div');
+	dom.id = input;
+	dom.style.width = "100px";
+	dom.style.height = "100px";
+	dom.style.background="green"
+	let img = new Image();
+	img.src = './meal2.jpg'
+	img.width = "100px"
+	img.height= "100px"
+	img.alt="new food"
+	dom.appendChild(img)
+	document.getElementById('bookings-orders').appendChild(dom)
+}

18681d7feb32134c85540010abee84a697537b16
phemy <phemy.smith@gmail.com>
2018-04-24 02:49:46 +0100

    route for update

--- Server/src/routes/api.js
+++ Server/src/routes/api.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { AllMeals, AddMeal } from '../controllers/';
+import { AllMeals, AddMeal, UpdateMeal } from '../controllers/';
 
 const router = express.Router();
 
 router.get('/meals', AllMeals.all);
 router.post('/meals', AddMeal.add);
+router.put('/meals/:id', UpdateMeal.update);
 
 export default router;

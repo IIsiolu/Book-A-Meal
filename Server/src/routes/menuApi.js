a484246d6e62c593ecd4b1496763659631131991
phemy <phemy.smith@gmail.com>
2018-04-24 15:18:44 +0100

    added menu routes to route

--- Server/src/routes/menuApi.js
+++ Server/src/routes/menuApi.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { PostMenu } from '../controllers/Menu';
+import { PostMenu, GetMenu } from '../controllers/Menu';
 
 const menuRouter = express.Router();
 
 menuRouter.post('/', PostMenu.today);
+menuRouter.get('/', GetMenu.menus)
+
 
 export default menuRouter;

81238b36c5f78d185d03bea0266b181d731b4a7f
phemy <phemy.smith@gmail.com>
2018-04-24 03:02:57 +0100

    route to delete meals\

--- Server/src/routes/api.js
+++ Server/src/routes/api.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { AllMeals, AddMeal, UpdateMeal } from '../controllers/';
+import { AllMeals, AddMeal, UpdateMeal, RemoveMeal } from '../controllers/';
 
 const router = express.Router();
 
 router.get('/meals', AllMeals.all);
 router.post('/meals', AddMeal.add);
 router.put('/meals/:id', UpdateMeal.update);
-
+router.delete('/meals/:mealId', RemoveMeal.delet)
 export default router;

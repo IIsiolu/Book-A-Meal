dc39cfda38ec3de1807cbc5331e83df4f42d68db
phemy <phemy.smith@gmail.com>
2018-04-28 22:58:35 +0100

    route for signin

--- server/routes/userApi.js
+++ server/routes/userApi.js
@@ -5,5 +5,6 @@ import { UserController } from '../controllers';
 const userRouter = express.Router();
 
 userRouter.post('/user', Validate.validateSignUp, UserController.signup);
+userRouter.post('/user/signin', UserController.signin);
 
 export default userRouter;

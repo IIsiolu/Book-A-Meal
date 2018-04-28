63fd455d1f4a588724292f300673d28373ce94a1
phemy <phemy.smith@gmail.com>
2018-04-28 01:01:44 +0100

    added middlewares

---
+++ server/middleware/auth.js
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken';
+
+require('dotenv').config();
+
+
+/** Class Auth a point. */
+class Auth {
+
+  static verifyToken(req, res, next) {
+    //  token
+    const token = req.body.token || req.query.token || req.headers.authorization;
+    if (token) {
+      const secret = process.env.SECRET;
+      jwt.verify(token, secret, (err, data) => {
+        if (err) {
+          return res.status(401).json({
+            message: 'Authentication failed',
+          });
+        }
+        req.user = data;
+        next();
+      });
+    } else {
+      // return 403 if token is not present
+      return res.status(403).json({
+        message: 'You need to sign up or login',
+      });
+    }
+  }
+}
+
+
+export default Auth;

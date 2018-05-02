777fefe6c1c7a39677c8ab337f97b2290948d348
phemy <phemy.smith@gmail.com>
2018-05-02 14:22:46 +0100

    added validation as middleware

--- server/routes/mealApi.js
+++ server/routes/mealApi.js
@@ -1,9 +1,9 @@
 import express from 'express';
-// import { Validate } from '../middleware';
+import { Validate } from '../middleware';
 import { MealController } from '../controllers';
 
 const mealRouter = express.Router();
 
-mealRouter.post('/meals', MealController.createMeal);
+mealRouter.post('/meals', Validate.validatemealInput, MealController.createMeal);
 
 export default mealRouter;

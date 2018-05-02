3b1dc4e9b943bec95277e87a3bda26f850b4afa5
phemy <phemy.smith@gmail.com>
2018-05-02 15:33:42 +0100

    updated meal route

--- server/routes/mealApi.js
+++ server/routes/mealApi.js
@@ -5,5 +5,7 @@ import { MealController } from '../controllers';
 const mealRouter = express.Router();
 
 mealRouter.post('/meals', Validate.validatemealInput, MealController.createMeal);
+mealRouter.get('/meals', MealController.allMeals);
+mealRouter.put('/meal/:mealId', MealController.editMeal);
 
 export default mealRouter;

19ca28b2d403ee12630812faa0e79694195da121
phemy <phemy.smith@gmail.com>
2018-05-03 13:49:43 +0100

    added order to controller

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,5 +1,6 @@
 import UserController from './userController';
 import MealController from './mealController';
 import MenuController from './menuController';
+import OrderController from './orderController';
 
-export { UserController, MealController, MenuController };
+export { UserController, MealController, MenuController, OrderController };

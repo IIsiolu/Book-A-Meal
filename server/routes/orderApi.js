57add718b4db486fd5dcaab8452548210f347287
phemy <phemy.smith@gmail.com>
2018-05-04 09:58:47 +0100

    auth for priviledged admin

--- server/routes/orderApi.js
+++ server/routes/orderApi.js
@@ -1,11 +1,11 @@
 import express from 'express';
-// import { Validate } from '../middleware';
+import { Validate, Auth } from '../middleware';
 import { OrderController } from '../controllers';
 
 const orderRouter = express.Router();
 
-orderRouter.post('/', OrderController.createOrder);
-orderRouter.put('/:orderId', OrderController.modifyOrder);
-orderRouter.get('/', OrderController.allOrders);
+orderRouter.post('/', Auth.verifyToken, OrderController.createOrder);
+orderRouter.put('/:orderId', Auth.verifyToken, OrderController.modifyOrder);
+orderRouter.get('/', Auth.verifyAdmin, OrderController.allOrders);
 
 export default orderRouter;

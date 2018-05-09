73d308a050dab75076bc1512894b7e94a48df69b
phemy <phemy.smith@gmail.com>
2018-05-09 11:25:10 +0100

    added more fakers

---
+++ server/test/faker/menuFaker.js
@@ -0,0 +1,35 @@
+import faker from 'faker';
+
+const menuFaker = {
+  newMenu: {
+    mealId: 1,
+    date: '2018-01-01'
+
+  },
+  newMenu1: {
+    mealId: 2,
+    date: '2018-01-01'
+  },
+  newMenu2: {
+    mealId: 3,
+    date: '2018-01-01'
+  },
+  newMenu3: {
+    mealId: '',
+    date: '2018-01-01'
+  },
+  newMenu4: {
+    mealId: 1,
+    date: '',
+  },
+  newMenu5: {
+    mealId: 9,
+    date: '2018-01-01'
+  },
+  newMenu6: {
+    mealId: 6,
+    date: '2018-01-01'
+  },
+};
+
+export default menuFaker;

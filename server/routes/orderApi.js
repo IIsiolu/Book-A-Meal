4a4e435a1df89457a359beb617f009e1cbcebdc8
phemy <phemy.smith@gmail.com>
2018-05-09 13:07:17 +0100

    added validation as middleware

--- server/routes/orderApi.js
+++ server/routes/orderApi.js
@@ -4,7 +4,7 @@ import { OrderController } from '../controllers';
 
 const orderRouter = express.Router();
 
-orderRouter.post('/', Auth.verifyToken, OrderController.createOrder);
+orderRouter.post('/', Auth.verifyToken, Validate.validateOrder, OrderController.createOrder);
 orderRouter.put('/:orderId', Auth.verifyToken, OrderController.modifyOrder);
 orderRouter.get('/', Auth.verifyAdmin, OrderController.allOrders);
 
